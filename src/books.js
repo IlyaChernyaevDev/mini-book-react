54f66099d13438fb77b50a9044399b3a3b6b127c
Ilya Chernyaev <ilyachernyaevdev@gmail.com>
2021-03-07 20:03:15 +0300

    added buttons and events for buttons in the Book component and improved the file structure of the application

---
+++ src/books.js
@@ -0,0 +1,20 @@
+export const books = [
+  {
+    id: 1,
+    img: `https://images-na.ssl-images-amazon.com/images/I/81AKVMzInqL._AC_UL200_SR200,200_.jpg`,
+    title: "They Both Die at the End",
+    author: "Adam Silvera",
+  },
+  {
+    id: 2,
+    img: `https://images-na.ssl-images-amazon.com/images/I/81drfTT9ZfL._AC_UL200_SR200,200_.jpg`,
+    title: "The Cat in the Hat",
+    author: "Dr. Seuss",
+  },
+  {
+    id: 3,
+    img: `https://images-na.ssl-images-amazon.com/images/I/81LoD65Jy6L._AC_UL200_SR200,200_.jpg`,
+    title: "Later",
+    author: "Stephen King",
+  },
+];
--- src/index.js
+++ src/index.js
@@ -3,26 +3,8 @@ import ReactDom from "react-dom";
 
 import "./index.css";
 
-const books = [
-  {
-    id: 1,
-    img: `https://images-na.ssl-images-amazon.com/images/I/81AKVMzInqL._AC_UL200_SR200,200_.jpg`,
-    title: "They Both Die at the End",
-    author: "Adam Silvera",
-  },
-  {
-    id: 2,
-    img: `https://images-na.ssl-images-amazon.com/images/I/81drfTT9ZfL._AC_UL200_SR200,200_.jpg`,
-    title: "The Cat in the Hat",
-    author: "Dr. Seuss",
-  },
-  {
-    id: 3,
-    img: `https://images-na.ssl-images-amazon.com/images/I/81LoD65Jy6L._AC_UL200_SR200,200_.jpg`,
-    title: "Later",
-    author: "Stephen King",
-  },
-];
+import { books } from "./books";
+import Book from "./Book";
 
 function BookList() {
   return (
@@ -33,15 +15,5 @@ function BookList() {
     </section>
   );
 }
-
-const Book = ({ img, title, author }) => {
-  return (
-    <article className="book">
-      <img src={img} alt="book cover" />
-      <h1>{title}</h1>
-      <h4>{author}</h4>
-    </article>
-  );
-};
 
 ReactDom.render(<BookList />, document.getElementById("root"));
